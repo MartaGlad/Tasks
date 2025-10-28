945cefe1705f1079002b4c918c55e68c339305ab
Marta Gladysz <kicia.nunus@gmail.com>
2025-10-28 14:05:41 +0100

    17.4 Added TaskController class

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    //@RequestMapping(method = RequestMethod.GET, value = "")
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "task title", "task_content");
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited task title", "Task content");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto){
+
+    }
+
+
+
+}
