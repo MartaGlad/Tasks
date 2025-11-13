def0a25aced6d0ba8aa174586a3cc0eb7751b7ac
Marta Gladysz <kicia.nunus@gmail.com>
2025-11-13 21:15:33 +0100

    21.2 Added functionality related to RestTemplate

---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+public class TrelloClient {
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+    @Value("${trello.username}")
+    private String trelloUsername;
+
+    private final RestTemplate restTemplate;
+
+    private URI getUrl() {
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint+"/1/members/"+trelloUsername+"/boards")
+                .queryParam("key",trelloAppKey)
+                .queryParam("token",trelloToken)
+                .queryParam("fields", "name,id")
+                .build().encode().toUri();
+        return url;
+    }
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        TrelloBoardDto[] boardsResponse =  restTemplate.getForObject(getUrl(), TrelloBoardDto[].class);
+
+        return Optional.ofNullable(boardsResponse)
+                .map(Arrays::asList)
+                .orElse(Collections.emptyList());
+    }
+}
