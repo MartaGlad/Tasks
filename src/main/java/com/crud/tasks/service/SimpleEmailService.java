52485ff5b59c01fdf5ff1fcaca28972abe145a28
Marta Gladysz <kicia.nunus@gmail.com>
2025-11-22 19:13:54 +0100

    22.3 Added e-mail sending functionality

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SimpleEmailService {
+
+    private final JavaMailSender javaMailSender; //interfejs
+
+    public void send(final Mail mail) throws MailException {
+        log.info("Starting e-mail preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+            log.info("E-mail has been sent.");
+        } catch (MailException e) {
+            log.error("Failed to process e-mail sending: " + e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        Optional<String> ccOptional = Optional.ofNullable(mail.getToCc());
+        if(ccOptional.isPresent() && !ccOptional.get().isEmpty()) {
+            mailMessage.setCc(ccOptional.get());
+        }
+
+
+        return mailMessage;
+    }
+}
