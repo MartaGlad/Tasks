7fc8d391af15e7c840c07b1900588402bd4612a0
Marta Gladysz <kicia.nunus@gmail.com>
2026-01-08 18:03:21 +0100

    30.1 Added StaticWebPageController and index.html

--- src/main/java/com/crud/tasks/domain/AttachmentsByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentsByType.java
@@ -1,7 +1,6 @@
 package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
